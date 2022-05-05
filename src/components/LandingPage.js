1c6954bd9a23efc3a7a3eb0756b7c356e786649a
Michael Neis <michael.neis12@gmail.com>
2022-05-05 14:30:48 -0500

    skeleton for landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
+function LandingPage(){
+    let history = useNavigate()
+
+    function handleLogin () {
+        history('/login')
+    }
+    function handleSignUp () {
+        history('/signup')
+    }
+
+
+    return(
+        <div>
+            <h1 className="landing-title">EXP Share</h1>
+            <div className="login-buttons">
+            <button className="nes-btn is-primary" onClick={handleLogin} style={{marginRight: '5%'}}>Login</button>
+            <button className="nes-btn is-success" variant="info" onClick={handleSignUp} style={{marginLeft: '5%'}}>Signup</button>
+            </div>
+            <h1>hello there</h1>
+        </div>
+    )
+}
+
+export default LandingPage
